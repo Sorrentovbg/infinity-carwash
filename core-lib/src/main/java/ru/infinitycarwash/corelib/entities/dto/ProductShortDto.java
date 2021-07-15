bd1111a8e35ae1fdb65a66943a6806b3ed29b1af
Maksim <fors76@mail.ru>
2021-07-15 22:47:29 +0300

    Commit Final(1)

---
+++ core-lib/src/main/java/ru/infinitycarwash/corelib/entities/dto/ProductShortDto.java
@@ -0,0 +1,14 @@
+package ru.infinitycarwash.corelib.entities.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ProductShortDto {
+
+    private String productName;
+
+    private String productShortDescription;
+
+}
